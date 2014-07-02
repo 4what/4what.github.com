4d23799e498b7657ee85446f6e1396b6b177f9ba
4what <4what@163.com>
2014-07-02 10:19:56 +0800

    Update

---
+++ cn/js/search.js
@@ -0,0 +1,31 @@
+﻿(function() {
+
+/* jQuery */
+$(function() {
+
+	// subnav
+	$("#subnav li a").each(function() {
+		var
+		item = $(this).find("span"),
+		name = $(this).attr("name"),
+		pathname = window.top.location.pathname;
+		if ($4what.file(name).test(pathname) || (name === "index" && /\/$/.test(pathname))) {
+			$(this).find("em").css("background", item.css("background-color")).addClass("highlight");
+		} else {
+			item = item.andSelf();
+			function handler(sign) {
+				item.animate({
+					"width": sign + "=15"
+				}, "fast");
+			}
+			$(this).mouseenter(function() {
+				handler("+");
+			}).mouseleave(function() {
+				handler("-");
+			});
+		}
+	});
+
+});
+
+})();
