4dfde7e73e4784cc8455349724c629c78dd8de30
4what <4what@163.com>
2015-11-25 15:14:09 +0800

    Update

--- example/extjs/js/admin.js
+++ example/extjs/js/admin.js
@@ -4,7 +4,7 @@ Ext.BLANK_IMAGE_URL = "js/extjs/3.4.1.1/resources/images/default/s.gif";
 // chart
 Ext.chart.Chart.CHART_URL = "js/extjs/3.4.1.1/resources/charts.swf";
 
-// MessageBox
+// msg
 $ext.msg.alert(/*{
 	timeout: function(o, title, msg) {
 		return !title ? 1000 : false;
