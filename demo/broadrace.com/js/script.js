cf23e13cd1be177e78073a6af0b83f794656d349
4what <4what@163.com>
2016-06-16 19:09:31 +0800

    Demo

--- demo/broadrace.com/js/script.js
+++ demo/broadrace.com/js/script.js
@@ -1,4 +1,10 @@
-﻿$(".goto-top").click(function() {
+﻿new Swiper(".swiper-container", {
+	nextButton: ".swiper-button-next",
+	prevButton: ".swiper-button-prev"
+});
+
+
+$(".goto-top").click(function() {
 	$("html, body").animate({
 		scrollTop: 0
 	}, 500);
