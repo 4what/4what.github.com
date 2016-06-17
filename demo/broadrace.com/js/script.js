0f24878053e296bcddaaf562196b5a1f22740c88
4what <4what@163.com>
2016-06-17 19:21:57 +0800

    Demo

--- demo/broadrace.com/js/script.js
+++ demo/broadrace.com/js/script.js
@@ -1,5 +1,19 @@
 ﻿$(function() {
 
+	$(".slick").slick({
+		prevArrow: ".slick-prev",
+		nextArrow: ".slick-next",
+
+		speed: 200
+	}).on("afterChange", function(event, slick, currentSlide, nextSlide) {
+	});
+
+
+	$(".service ul li").hover(function() {
+		$("img, h3", this).toggleClass("highlight");
+	});
+
+
 	var qrcode = $(".qrcode");
 
 	$(".action-qrcode").add(qrcode).hover(function() {
@@ -17,15 +31,6 @@
 		$("html, body").animate({
 			scrollTop: 0
 		}, 500);
-	});
-
-
-	$(".slick").slick({
-		prevArrow: ".slick-prev",
-		nextArrow: ".slick-next",
-
-		speed: 200
-	}).on("afterChange", function(event, slick, currentSlide, nextSlide) {
 	});
 
 });
