5069978ffde9306123fe67a88f6bbe9f793eea70
4what <root@4what.cn>
2021-11-03 19:38:40 +0800

    no message

--- sample/20451652-B_3rd_Gen_AMD_Ryzen_Threadripper/course/nav-menu-disable.js
+++
@@ -1,7 +0,0 @@
-(function() {
-	Monsoon.Nav.__updateNavButtons = Monsoon.Nav.updateNavButtons;
-	Monsoon.Nav.updateNavButtons = (function() {
-		$('#dropdownMenuNavigation').attr('disabled', !this.enabled);
-		Monsoon.Nav.__updateNavButtons();
-	}).bind(Monsoon.Nav);
-})();
